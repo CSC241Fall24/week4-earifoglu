5e42e39519f87f97aa83c1a44597f6dbb59d581c
earifoglu <143426905+earifoglu@users.noreply.github.com>
2024-09-25 20:28:33 -0400

    Update Problem1.java

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,11 +1,17 @@
 // Problem1.java
 public class Problem1 {
     public static ListNode concatenate(ListNode l1, ListNode l2) {
-        ListNode ref = l1;
-        while(ref.next != null) {
-            ref = ref.next;
+        try {
+            
+            ListNode ref = l1;
+            while(ref.next != null) {
+                ref = ref.next;
+            }
+            ref.next = l2;
+            return l1;
+        } 
+        catch(NullPointerException e) {
+            return l1;
         }
-        ref.next = l2;
-        return l1;
     }
 }
